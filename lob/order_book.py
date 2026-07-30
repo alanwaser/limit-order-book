3af390a38e164ce5d7de210ae3fc03736939c06e
Alan Antony <ala96@aber.ac.uk>
2026-07-30 20:16:14 +0100

    Add initial orderbook code

---
+++ lob/order_book.py
@@ -0,0 +1,24 @@
+from collections import OrderedDict
+from sortedcontainers import SortedDict
+from lob.order import Order, Side, OrderType
+
+
+class LimitOrderBook:
+
+    def __init__(self):
+        self.bids = SortedDict()
+        self.asks = SortedDict()
+        self.order_lookup = {} # flat cancel lookup
+
+    def get_bestBid(self):
+        maxBid = self.bids.peekitem(-1)
+        return maxBid
+
+    def get_bestAsk(self):
+        maxAsk = self.asks.peekitem(-1)
+        return maxAsk
+    
+    def get_order_book(self, order_id):
+        return self.order_lookup[order_id]
+
+
